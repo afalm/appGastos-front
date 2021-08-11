3c8acb741d07d45f5ff387b01abbf1356161ac46
Alberto Fernandez <alberto.falm@gmail.com>
2021-08-11 11:17:57 +0200

    Añade en la tabla el numero total

--- src/app/components/expenses/expenses.component.ts
+++ src/app/components/expenses/expenses.component.ts
@@ -7,30 +7,34 @@ import { ExpensesService } from '../../services/expenses.service';
   styleUrls: ['./expenses.component.css']
 })
 export class ExpensesComponent implements OnInit {
-  total = 0
+  total: number;
+  
+  
   constructor(public expenseService: ExpensesService){}
   
   ngOnInit(): void {
     this.getExpenses();
-    //total = this.getTotal(this.expenseService.expenses)
+    
+    
   }
   getTotal(expenses: Expense[]){
     var num: number = 0;
     for(var i: number = 0; i < expenses.length; i++){// obtener el total de gastos/ ingresos
-      num += i;
+      num += expenses[i].quantity;
     }
     return num;
   }
   getExpenses(){
-    var expenses: Expense[]
+    
     this.expenseService.getExpneses().subscribe(
       res => {
         this.expenseService.expenses = res,
-        expenses = res
+        this.total = this.getTotal(this.expenseService.expenses),
+        console.log(this.total)
       },
       err => console.log(err)
       )
-    
+      
   }
   deleteExpense(id: string){
     if(confirm('¿Está seguro de querer elimiar el gasto?')){
